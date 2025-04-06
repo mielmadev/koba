420b568b9065535aa90a6cc1130c7f207732d50f
MielmaDev <elizabethmaranon80@gmail.com>
2025-04-06 17:33:22 +0200

    Agregar altura dinámica al encabezado, mejorar estilos de la página de inicio y ajustar variables de estilo

--- src/components/paginas/inicio-pag.jsx
+++ src/components/paginas/inicio-pag.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import './inicio-pag.scss'
-import Encabezado from '../encabezado-pie/encabezado'
-
+import React from 'react';
+import './inicio-pag.scss';
+import Volcan from '../../assets/imagenes/fondos/volcan.jpg';
 
 export default function InicioPag() {
   return (
     <div className="inicio-container">
-      <h1>pagina de inicio, </h1>
-      <h2> elemento pie página</h2>
+      <div className="inicio-fondo">
+        <img src={Volcan} alt="Volcán" />
       </div>
-  )
+      <div className="inicio-contenido">
+      <h1>pagina de inicio</h1>
+        <h2>elemento pie página</h2>
+        </div>
+    </div>
+  );
 }
