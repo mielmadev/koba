324c5b57267b17aeff75b585ac35e237e1c4f667
MielmaDev <elizabethmaranon80@gmail.com>
2025-04-07 17:29:28 +0200

    Agregar componente de bandas en la página de inicio, ajustar estilos y estructura para mejorar la presentación y responsividad.

--- src/components/paginas/inicio-pag.jsx
+++ src/components/paginas/inicio-pag.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './inicio-pag.scss';
 import Volcan from '../../assets/imagenes/fondos/volcan1620x1080.jpg';
+import Suicidal from '../../assets/imagenes/bandas/Suicidal_Angels_Logo.png';
 
 export default function InicioPag() {
   return (
@@ -9,9 +10,39 @@ export default function InicioPag() {
         <img src={Volcan} alt="Volcán" />
       </div>
       <div className="inicio-contenido">
-      <h1>pagina de inicio</h1>
-        <h2>elemento pie página</h2>
+        <div className="fecha">
+          <h1>Fecha</h1>
         </div>
+        <div className='bandas-container'>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+          <div className='bandas'>
+            <img src={Suicidal} alt="Volcán" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
