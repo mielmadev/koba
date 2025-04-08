453c4e33a80baedf1ab1c81f2a845ca53a424a9a
MielmaDev <elizabethmaranon80@gmail.com>
2025-04-08 23:40:13 +0200

    Refactor componente Bandas para mejorar la presentación, eliminar elementos innecesarios y agregar estilos. Actualizar mixins para consistencia en el diseño.

--- src/components/bandas/bandas.jsx
+++ src/components/bandas/bandas.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import bandas from '../../../data/bandas';
+import './bandas.scss';
 
 const Bandas = () => {
   return (
-    <div>
-      <h1>Bandas</h1>
-      <ul>
-        {bandas.map((banda, index) => (
-          <li key={index}>
-            <img src={banda.imagen} alt={banda.nombre} style={{ width: '100px', height: '100px' }} />
-            <p>{banda.nombre}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="bandas">
+      {bandas.map((banda, index) => (
+        <div key={index} className="banda-item">
+          <img src={banda.imagen} alt={banda.nombre} style={{ width: '100px', height: '100px' }} />
+          <p>{banda.nombre}</p>
+        </div>
+      ))}
     </div>
   );
 };
