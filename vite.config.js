536e5889246619255a870cccf603ee96146d213a
MielmaDev <elizabethmaranon80@gmail.com>
2025-04-11 22:25:29 +0200

    Actualizar dependencias en package.json y ajustar configuración en vite.config.js para mejorar la estructura del proyecto y asegurar la correcta generación de archivos de salida.

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   publicDir: 'public', // Asegura que Vite use la carpeta public
   server: {
     open: true, // Abre el navegador automáticamente al iniciar el servidor
+    port: 3000, // Puerto para desarrollo
   },
+  build: {
+    outDir: 'dist', // Carpeta de salida
+    sourcemap: true, // Opcional: genera mapas de fuente
+  },
+  base: '/koba/', // Reemplaza <nombre-del-repositorio> con el nombre de tu repositorio
 })
