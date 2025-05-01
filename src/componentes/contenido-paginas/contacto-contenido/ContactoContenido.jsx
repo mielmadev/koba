f57af7c54e8532b9bbb1aaed5cd8b28b9d5af469
MielmaDev <elizabethmaranon80@gmail.com>
2025-05-01 13:19:38 +0200

    Agregar react-icons y actualizar estilos en componentes de contenido

--- src/componentes/contenido-paginas/contacto-contenido/ContactoContenido.jsx
+++ src/componentes/contenido-paginas/contacto-contenido/ContactoContenido.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaSquareEnvelope } from "react-icons/fa6";
+import './ContactoContenido.scss';
 
-export default function ContacoContenido() {
+
+
+export default function ContactoContenido() {
   return (
-    <div>
-      contacto contenido
+    <div className='contenido'>
+      <div className='contacto'>
+        <div className='whatsapp'>
+          <span className='whatsapp-icon'><FaWhatsappSquare /></span>
+          <span className='whatsapp-text'> +34 12345678</span>
+        </div>
+        <div className='email'>
+          <span className='email-icon'><FaSquareEnvelope /></span>
+          <span className='email-text'> info@koba-live.com</span>
+        </div>
+      </div>
+
     </div>
   );
 }
