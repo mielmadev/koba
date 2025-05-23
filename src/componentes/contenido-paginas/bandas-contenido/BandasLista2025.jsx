bee5aee2c97fb2af3863b98b3b34436a745c67bd
MielmaDev <elizabethmaranon80@gmail.com>
2025-05-23 19:08:12 +0200

    Refactor estilos de grupos y bandas: optimizar la estructura de SCSS en gruposContenido y BandasLista2025. Añadir estilos para bandas-lista y ajustar la presentación en componentes relacionados.

--- src/componentes/contenido-paginas/bandas-contenido/BandasLista2025.jsx
+++ src/componentes/contenido-paginas/bandas-contenido/BandasLista2025.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import bandasDatos2025 from "@datos/bandasDatosAnio2025"
+import "./BandasLista2025.scss"
 
 const BandasLista2025 = () => {
   // Ordenar alfabéticamente por nombre
   const bandasOrdenadas = [...bandasDatos2025].sort((a, b) => a.nombre.localeCompare(b.nombre))
 
   return (
-    <div className="contenido">
+    <div className="bandas-lista">
       {bandasOrdenadas.map((banda, index) => (
         <div key={index} className="banda">
           <div className="banda-nombre">
