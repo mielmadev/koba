903fb162c16d156a81eba44a2e67b0ae037f7f27
MielmaDev <elizabethmaranon80@gmail.com>
2025-05-25 09:39:44 +0200

    Agregar iconos separadores y mejorar estilos en componente Fecha para mejorar la presentación visual.

--- src/componentes/fecha/fecha.jsx
+++ src/componentes/fecha/fecha.jsx
@@ -1,12 +1,24 @@
 import React from "react"
 import { fecha, ubicacion } from "@datos/fechaDatos"
 import "./fecha.scss"
+import MetalSeparadorIcono from "../iconos/MetalSeparadorIcono"
+import EscudoSeparadorIcono from "../iconos/EscudoSeparadorIcono"
 
 const Fecha = () => {
   return (
     <div className="fecha-contenedor">
       <div className="fecha">
-        {fecha} - {ubicacion}
+        <span className="fecha-svg-separador" style={{ display: 'inline-flex', alignItems: 'center', margin: '0 0.5em' }}>
+          <EscudoSeparadorIcono size={48} />
+        </span>
+        {fecha}
+        <span className="fecha-svg-separador" style={{ display: 'inline-flex', alignItems: 'center', margin: '0 0.5em' }}>
+          <MetalSeparadorIcono size={48} />
+        </span>
+        {ubicacion}
+        <span className="fecha-svg-separador" style={{ display: 'inline-flex', alignItems: 'center', margin: '0 0.5em' }}>
+          <EscudoSeparadorIcono size={48} />
+        </span>
       </div>
     </div>
   )
