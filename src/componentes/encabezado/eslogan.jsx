b3cdc5c186b6786640ea78d32ec71688dcdd5c11
MielmaDev <elizabethmaranon80@gmail.com>
2025-05-31 09:54:42 +0200

    Refactor Koba IV: mejorar el componente Eslogan para incluir el botón de entradas y ajustar estilos de alineación en el contenedor

--- src/componentes/encabezado/eslogan.jsx
+++ src/componentes/encabezado/eslogan.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import "./Eslogan.scss"
+import BotonEntradas from "../utils/BotonEntradas"
 
 const Eslogan = () => {
-  return <div className="eslogan-contenedor">Where Metal Comes True</div>
+  return <div className="eslogan-contenedor">
+    <div className="eslogan">Where Metal Comes True</div>
+    <div className="entradas">
+      <BotonEntradas href="#entradas">Entradas</BotonEntradas>
+    </div>
+  </div>
 }
 
 export default Eslogan
