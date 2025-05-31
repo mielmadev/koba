c8318670fd9818d4b202d751761ffa5962eedb4b
MielmaDev <elizabethmaranon80@gmail.com>
2025-05-31 10:57:21 +0200

    Refactor Koba IV: agregar estilos globales para el iframe de entradas y mejorar el botón de entradas con nueva fuente y padding

--- src/componentes/utils/IframeEntradas.jsx
+++ src/componentes/utils/IframeEntradas.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 const IframeEntradas = () => (
-  <div style={{ width: "100%", maxWidth: 600, margin: "0 auto" }}>
-    <iframe
-      data-src="https://venta.enterticket.es/buy/?id=40288&color=000000"
-      id="etktfrm40288"
-      width="100%"
-      frameBorder="0"
-      allowFullScreen
-      src="https://venta.enterticket.es/buy/?id=40288&color=000000"
-      style={{ height: 357, border: "none" }}
-      title="Entradas KOBA Live"
-    ></iframe>
-  </div>
+  <iframe
+    className="iframe-entradas"
+    data-src="https://venta.enterticket.es/buy/?id=40288&color=000000"
+    id="etktfrm40288"
+    width="100%"
+    frameBorder="0"
+    allowFullScreen
+    src="https://venta.enterticket.es/buy/?id=40288&color=000000"
+    title="Entradas KOBA Live"
+  ></iframe>
 );
 
 export default IframeEntradas;
