ec070b1045f3be58c6d7b5572c6ce9e7df87aa84
MielmaDev <elizabethmaranon80@gmail.com>
2025-06-25 15:33:17 +0200

    feat: actualizar descripción y ajustar tamaños de fuente en el componente MielmaDev

--- src/componentes/contenido-paginas/mielmadev-contenido/MielmaDev.jsx
+++ src/componentes/contenido-paginas/mielmadev-contenido/MielmaDev.jsx
@@ -7,7 +7,7 @@ const MielmaDev = () => (
       <div className="mielmadev-titulo">MielmaDev Full Stack Developer</div>
       <div className="mielmadev-textos">
         <div className="mielmadev-descripcion">
-          <span className="mielmadev-servicio">Creamos páginas web a medida para negocios, profesionales y emprendedores.</span><br />
+          <span className="mielmadev-servicio">Creamos páginas web a medida para profesionales y emprendedores</span><br />
           Soluciones profesionales en desarrollo web. Especialista en aplicaciones modernas, escalables y seguras para empresas y proyectos que buscan excelencia tecnológica.
         </div>
         <div className="mielmadev-eslogan">
